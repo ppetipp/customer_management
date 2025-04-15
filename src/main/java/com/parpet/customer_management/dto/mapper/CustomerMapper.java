88b51f023531dc4208ba2e8b0b64db1781b8fc95
Parancs Péter <parancs.peter@gmail.com>
2025-04-15 11:05:22 +0200

    entitiesToDto fix

--- src/main/java/com/parpet/customer_management/dto/mapper/CustomerMapper.java
+++ src/main/java/com/parpet/customer_management/dto/mapper/CustomerMapper.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Component;
 public class CustomerMapper {
     public CustomerDetails entitiesToDto(Customer customer) {
         CustomerDetails customerDetails = new CustomerDetails();
+        customerDetails.setId(customer.getId());
         customerDetails.setName(customer.getName());
         customerDetails.setAge(customer.getAge());
         customerDetails.setDateOfBirth(customer.getDateOfBirth());
